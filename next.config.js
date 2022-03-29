39abc0b408c0b07b68929f0d2c0719e6c02c2419
Efraa <efra23peralta@gmail.com>
2022-03-29 08:22:24 -0400

    feat: get client environment vars at runtime

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    APP_URL: process.env.APP_URL,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+  },
 }
